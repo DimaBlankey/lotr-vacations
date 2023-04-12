3d839b2898bae204e959535c3adfa7724e149601
DimaBlankey <115228381+DimaBlankey@users.noreply.github.com>
2023-04-12 12:15:04 +0300

    404

--- Frontend/src/Components/LayoutArea/PageNotFound/PageNotFound.tsx
+++ Frontend/src/Components/LayoutArea/PageNotFound/PageNotFound.tsx
@@ -1,15 +1,15 @@
 import "./PageNotFound.css";
-import video from "../../../Assets/videos/video.mp4";
+import video2 from "../../../Assets/videos/video2.mp4";
 
 function PageNotFound(): JSX.Element {
   return (
     <div className="PageNotFound">
       <>
         <video autoPlay muted loop id="myVideo">
-          <source src={video} type="video/mp4" />
+          <source src={video2} type="video/mp4" />
         </video>
         <div className="content">
-          <h1>404</h1>
+          <h1>Page Not Found...404</h1>
         </div>
       </>
     </div>
