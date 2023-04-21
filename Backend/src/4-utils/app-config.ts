92ba24710c192cf70a18a3d67d8fb992895c0190
DimaBlankey <115228381+DimaBlankey@users.noreply.github.com>
2023-04-21 11:34:19 +0300

    update

--- Backend/src/4-utils/app-config.ts
+++ Backend/src/4-utils/app-config.ts
@@ -1,31 +1,3 @@
-// class AppConfig {
-
-//     // Server Port:
-//     public port = 4000;
-
-//     // Server Url:
-//     public serverUrl = "http://localhost:" + this.port;
-
-//     // Images Url:
-//     public imagesUrl = this.serverUrl + "/api/vacations/images/"
-
-//     // Database Host (on which computer the database exists):
-//     public mySqlHost = "localhost";
-
-//     // Database User
-//     public mySqlUser = "root";
-
-//     // Database Password: 
-//     public mySqlPassword = "";
-
-//     // Database Name: 
-//     public mySqlDatabase = "lotr-vacations"; // Fill in database name
-// }
-
-// const appConfig = new AppConfig();
-
-// export default appConfig;
-
 class AppConfig {
 
     // Server Port:
