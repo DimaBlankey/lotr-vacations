53ff61ffdf50f702cce855dda3638a7512a5bc8a
DimaBlankey <115228381+DimaBlankey@users.noreply.github.com>
2023-04-21 10:58:34 +0300

    docker

--- Backend/src/4-utils/app-config.ts
+++ Backend/src/4-utils/app-config.ts
@@ -1,7 +1,35 @@
+// class AppConfig {
+
+//     // Server Port:
+//     public port = 4000;
+
+//     // Server Url:
+//     public serverUrl = "http://localhost:" + this.port;
+
+//     // Images Url:
+//     public imagesUrl = this.serverUrl + "/api/vacations/images/"
+
+//     // Database Host (on which computer the database exists):
+//     public mySqlHost = "localhost";
+
+//     // Database User
+//     public mySqlUser = "root";
+
+//     // Database Password: 
+//     public mySqlPassword = "";
+
+//     // Database Name: 
+//     public mySqlDatabase = "lotr-vacations"; // Fill in database name
+// }
+
+// const appConfig = new AppConfig();
+
+// export default appConfig;
+
 class AppConfig {
 
     // Server Port:
-    public port = 4000;
+    public port = process.env.PORT || 4000;
 
     // Server Url:
     public serverUrl = "http://localhost:" + this.port;
@@ -10,16 +38,16 @@ class AppConfig {
     public imagesUrl = this.serverUrl + "/api/vacations/images/"
 
     // Database Host (on which computer the database exists):
-    public mySqlHost = "localhost";
+    public mySqlHost = process.env.MYSQL_HOST || "localhost";
 
     // Database User
-    public mySqlUser = "root";
+    public mySqlUser = process.env.MYSQL_USER || "root";
 
     // Database Password: 
-    public mySqlPassword = "";
+    public mySqlPassword = process.env.MYSQL_PASSWORD || "";
 
     // Database Name: 
-    public mySqlDatabase = "lotr-vacations"; // Fill in database name
+    public mySqlDatabase = process.env.MYSQL_DATABASE || "lotr-vacations";
 }
 
 const appConfig = new AppConfig();
